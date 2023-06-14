28a69fb06b52c1dc3065f25bd79db6d9003a5490
Pjoter <piotr.janiuk@yahoo.pl>
2023-06-14 14:24:24 +0200

    Dodaj pliki projektów.

---
+++ Aplikacja MVC/Models/DataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplikacja_MVC.Models
+{
+    
+        public class DataModel
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    
+}
